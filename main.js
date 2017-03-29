d105177436631bf76cf87c1b378cf9c9df87145f
Dairon Rodriguez <drodriguez@fvi.edu>
2017-03-29 17:43:33 -0400

    starting

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-$.ajax({
-    url: "http://api.open-notify.org/astros.json",
-    method: 'GET',
-    success: function(space) {
-        $('#astronauts').html("")
-        $('#astronauts').append(`<tr><td>${space.name}</td>, <td>${space.craft}</td></tr>`)
-
-    },
-});
+$(document).ready(function() {
 
 
-$.ajax({
-    url: 'http://api.open-notify.org/astros.json',
-    method: "POST",
-    success: function() {
-    console.log("yous data is been post")
-},
-data: {
-    astronauts: $('#astronauts').val(),
+    $.ajax({
+        url: "http://api.open-notify.org/astros.json",
+        method: 'GET',
+        success: function(space) {
+            console.log(space);
+            spacePeople(space.people);
+        },
+    });
 
-     },
+    function spacePeople(people) {
+        for (var i = 0; i < people.length; i++) {
+            var name = '<p class="' + 'name' + i + '">' + people[i].name + '</p>';
+            var craft = '<p class="' + 'craft' + i + '">' + people[i].craft + '</p>';
+            $('#astronauts').append(name);
+            $('#astronauts').append(craft);
+        }
+    };
+
 });
